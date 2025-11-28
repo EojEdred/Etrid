2ea14e16498c10f052dd0e03451ecd4c0dbf30d5
Eoj Edred <eojedredbitepubkey1@proton.me>
2025-11-27 21:17:59 -0600

    Add EtridWalletSwift directory

---
+++ EtridWalletSwift/EtridWalletSwiftTests/EtridWalletSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  EtridWalletSwiftTests.swift
+//  EtridWalletSwiftTests
+//
+//  Created by macbook on 11/22/25.
+//
+
+import Testing
+
+struct EtridWalletSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
